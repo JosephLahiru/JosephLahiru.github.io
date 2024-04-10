925ba348d341d1dbdbce736029124a5be058324d
theAlphaGuardian <joseph.rasanjana1@gmail.com>
2024-04-10 11:16:21 +0000

    added Charity tracking

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
-  return (
+ return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          id="clarity"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ludxxofh0q");
+            `,
+          }}
+        />
+      </Head>
       <body>
+        {/* Your existing theme switcher script */}
         <Script id='theme-switcher' strategy='beforeInteractive'>
           {`
             if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -19,5 +34,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+ );
 }
